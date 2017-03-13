5c15afc8278b626580d350a97fd7233a0c6a32bf
Jack Wang <wfenhan@gmail.com>
2017-03-12 22:57:16 -0400

    知道怎么改模板了，其实可以直接新建一个

---
+++ app/overrides/registration_future.rb
@@ -0,0 +1,15 @@
+# Deface::Override.new(virtual_path: 	"spree/shared/_products",
+#                      insert_before: "div#products",
+#                      text: 			"<p>Registration is the future!</p>",
+#                      name: 			"registration_future"
+#                    )
+# Deface::Override.new(virtual_path: 	"spree/shared/_products",
+#                      insert_before: "div#products",
+#                      text: 			"<p>Registration is the future!</p>",
+#                      name: 			"registration_future"
+#                    )
+# Deface::Override.new(virtual_path: "spree/shared/_products",
+#                      remove: "[data-hook='homepage_products']",
+#                      name: "registration_future"
+#                      spree/checkout/registration
+#                    )
