863fbb891e7d263693ecac73bf8e6d74281c0f89
Jack Wang <wfenhan@gmail.com>
2017-09-25 22:22:36 -0400

    Push new staff to github

---
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -0,0 +1,14 @@
+module Spree
+  module FrontendHelper
+    def taxons_tree(root_taxon, current_taxon, max_level = 1)
+      return '' if max_level < 1 || root_taxon.leaf?
+      content_tag :div, class: 'list-group' do
+        taxons = root_taxon.children.map do |taxon|
+          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
+          link_to(image_tag("#{taxon.name}.png", border: 0), seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+        end
+        safe_join(taxons, "\n")
+      end
+    end
+  end
+end
